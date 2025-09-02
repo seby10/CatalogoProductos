1ceb73b1aa845cba61202eaba883728d9075005b
Pablo Montero <pmontero4909@uta.edu.ec>
2025-09-02 16:36:21 -0500

    Add Service

---
+++ Services/IProductoService.cs
@@ -0,0 +1,14 @@
+﻿using CatalogoProductos.Models;
+
+namespace CatalogoProductos.Services
+{
+    public interface IProductoService
+    {
+        Task<IEnumerable<Producto>> GetAllProductos();
+        Task<Producto?> GetProductoById(int id);
+        Task<Producto> CreateProducto(Producto producto);
+        Task<Producto?> UpdateProducto(int id, Producto producto);
+        Task<bool> DeleteProducto(int id);
+    }
+}
+
